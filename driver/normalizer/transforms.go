683655ced92a4983f415c6b0fce1a985002c7c26
Juanjo Alvarez <juanjo@sourced.tech>
2019-02-05 11:39:53 +0100

    Update to SDK 2.13.x
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>
    
    Update gopkg.lock
    
    Signed-off-by: Juanjo Alvarez <juanjo@sourced.tech>

--- driver/normalizer/transforms.go
+++ driver/normalizer/transforms.go
@@ -3,9 +3,10 @@ package normalizer
 import "gopkg.in/bblfsh/sdk.v2/driver"
 
 var Transforms = driver.Transforms{
-	Namespace:   "bash",
-	Preprocess:  Preprocess,
-	Normalize:   Normalize,
-	Annotations: Native,
-	Code:        Code,
+	Namespace:      "bash",
+	Preprocess:     Preprocess,
+	PreprocessCode: PreprocessCode,
+	Normalize:      Normalize,
+	Annotations:    Native,
+	Code:           Code,
 }
